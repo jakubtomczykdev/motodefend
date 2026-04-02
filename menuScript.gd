a0d30bc10391ab3c214a260742bf2fd52835839f
kubabb <kubarogoz3@gmail.com>
2026-04-02 22:44:29 +0200

    added settings menu

--- menuScript.gd
+++ menuScript.gd
@@ -1,17 +1,21 @@
 extends Control
 
-# Ścieżka do głównej sceny gry
-@export var game_scene_path : String = "res://scenes/World.tscn"
+# Ścieżki do scen (zwróć uwagę, czy na pewno Start ma prowadzić do "scenes/Settings.tscn")
+@export var game_scene_path : String = "res://scenes/Settings.tscn" 
+@export var settings_scene_path : String = "res://Settings.tscn"
+
 @onready var start_button: Button = $ButtonsContainer/StartButton
+
 func _ready():
 	start_button.grab_focus()
 
 func _on_start_button_pressed():
-
 	get_tree().change_scene_to_file(game_scene_path)
-
-func _on_options_button_pressed():
-	print("Otwieranie ustawień protokołów...")
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+# Tutaj podłączony jest Twój nowy przycisk "settingsBtn"
+func _on_settings_btn_pressed() -> void:
+	# Zamieniliśmy "pass" na zmianę sceny, używając Twojej zmiennej z góry!
+	get_tree().change_scene_to_file(settings_scene_path)
