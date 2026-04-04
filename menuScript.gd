3a0446615717acd4254dbac7d4401a858af070a8
kubabb <kubarogoz3@gmail.com>
2026-04-04 22:31:35 +0200

    added second scene and menu upgrades

--- menuScript.gd
+++ menuScript.gd
@@ -1,21 +1,29 @@
 extends Control
 
-# Ścieżki do scen (zwróć uwagę, czy na pewno Start ma prowadzić do "scenes/Settings.tscn")
-@export var game_scene_path : String = "res://scenes/Settings.tscn" 
+# Ścieżki do scen (dostosowane do Twojego projektu)
+@export var game_scene_path : String = "res://World.tscn" 
 @export var settings_scene_path : String = "res://Settings.tscn"
 
-@onready var start_button: Button = $ButtonsContainer/StartButton
+# Używamy unikalnej nazwy (%), żeby Godot sam znalazł przycisk, 
+# niezależnie od tego w ilu kontenerach jest schowany.
+@onready var start_button: Button = %StartButton
 
 func _ready():
+	# Ustawia fokus na przycisku "Start" po włączeniu menu
 	start_button.grab_focus()
 
+# --- PRZYCISK: START ---
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file(game_scene_path)
 
-func _on_exit_button_pressed():
+# --- PRZYCISK: USTAWIENIA ---
+func _on_setting_btn_pressed() -> void:
+	get_tree().change_scene_to_file(settings_scene_path)
+
+# --- PRZYCISK: WYJŚCIE ---
+func _on_button_2_pressed():
 	get_tree().quit()
 
-# Tutaj podłączony jest Twój nowy przycisk "settingsBtn"
+
 func _on_settings_btn_pressed() -> void:
-	# Zamieniliśmy "pass" na zmianę sceny, używając Twojej zmiennej z góry!
 	get_tree().change_scene_to_file(settings_scene_path)
